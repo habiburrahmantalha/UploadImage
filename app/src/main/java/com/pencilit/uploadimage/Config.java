2b79bff275ff320b72188ed4e8a04f1b805a552f
talha629 <talha629@gmail.com>
2016-10-09 01:05:30 +0600

    Initial commit

---
+++ app/src/main/java/com/pencilit/uploadimage/Config.java
@@ -0,0 +1,9 @@
+package com.pencilit.uploadimage;
+
+public class Config {
+	// File upload url (replace the ip with your server address)
+	public static final String FILE_UPLOAD_URL = "http://besafeubl.com/androidapp/fileUpload.php";
+	
+	// Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+}
